b09159900a9766f2b85cc80b1efa30ebfbdf6984
mricopratama <mricopratama13@gmail.com>
2025-05-06 22:13:26 +0700

    migrasi ke dinamis

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,14 +2,16 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Models\Menu;
+use App\Models\Promo;
+use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function index()
     {
+        $promos = Promo::all();
         $menus = Menu::all();
-        return view('welcome', compact('menus'));
+        return view('welcome', compact('menus', 'promos'));
     }
 }
